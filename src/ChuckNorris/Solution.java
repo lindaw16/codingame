79f70294bb1fd9a7f566e3b42367f18b5a403227
lindaw16 <Linda@Lindas-MacBook-Pro-2.local>
2015-02-18 21:38:48 -0500

    Chuck Norris

---
+++ src/ChuckNorris/Solution.java
@@ -0,0 +1,82 @@
+package ChuckNorris;
+
+import java.nio.charset.Charset;
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        //Scanner in = new Scanner(System.in);
+        //String MESSAGE = in.nextLine();
+        String MESSAGE = "Chuck Norris' keyboard has 2 keys: 0 and white space.";
+
+        char[] letters = MESSAGE.toCharArray();
+        String prevNum = "";
+        int counter = 0;
+
+        StringBuilder binaryRep = new StringBuilder();
+
+        //convert to binary
+        for (char c: letters) {
+            String cBinary = Integer.toString(c, 2);
+            // make sure they are length 7
+            while (cBinary.length() < 7) {
+                cBinary = "0" + cBinary;
+            }
+            binaryRep.append(cBinary);
+        }
+
+        char[] binaryMessage = binaryRep.toString().toCharArray();
+        StringBuilder encoding = new StringBuilder();
+
+        for (char c: binaryMessage) {
+            String s = c+"";
+            if (s.equals(prevNum)) {
+                //increment the counter and keep going
+                counter++;
+            } else {
+                //new number
+                if (!prevNum.isEmpty()) {
+                    // append the contents to the encoding
+                    encoding.append(addToEncoding(prevNum, counter) + " ");
+                    // reset counter
+                    counter = 1;
+                } else {
+                    // just starting
+                    counter++;
+                }
+            }
+            prevNum = s;
+        }
+
+        encoding.append(addToEncoding(prevNum, counter));
+        System.out.println(encoding.toString());
+    }
+
+
+
+    private static String addToEncoding(String prevNum, int counter) {
+        StringBuilder sb = new StringBuilder();
+        if (prevNum.equals("0")) {
+            sb.append("00");
+        } else if (prevNum.equals("1")) {
+            sb.append("0");
+        } else {
+            //throw new RuntimeException("invalid binary character: " + prevNum);
+        }
+
+        sb.append(" ");
+
+        for (int i = 0; i < counter; i++) {
+            sb.append("0");
+        }
+
+        return sb.toString();
+    }
+}
