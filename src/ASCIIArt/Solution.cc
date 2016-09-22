4f4ca9b85f1bffa09bad711a9be2812ec91fdf75
linda <lindaw16@mit.edu>
2016-09-22 09:07:25 -0700

    improved ascii solution

--- src/ASCIIArt/Solution.cc
+++ src/ASCIIArt/Solution.cc
@@ -2,8 +2,6 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <unordered_map>
-#include <vector>
 
 using namespace std;
 
@@ -20,26 +18,15 @@ int main()
     string T;
     getline(cin, T);
 
-    unordered_map <char, vector<string>> dictionary;
-    string alphabet = "abcdefghijklmnopqrstuvwxyz?";
-
     for (int row = 0; row < H; row++) {
         string ROW;
         getline(cin, ROW);
 
-        for (int letter = 0; letter < alphabet.length(); letter++) {
-            char c = alphabet[letter];
-            string text_data = ROW.substr(letter*L, L);
-            dictionary[c].push_back(text_data);
-        }
-    }
-
-    for (int i = 0; i < H; i++) {
         string text = "";
-
         for (char& c: T) {
-            char lowerC = tolower(c);
-            text += dictionary.find(lowerC) == dictionary.end() ? dictionary['?'][i] : dictionary[lowerC][i];
+            // If the letter is not part of the alphabet, set it to the index of "?"
+            int letter = isalpha(c) ? tolower(c) - 'a' : 26;
+            text += ROW.substr(letter*L, L);
         }
 
         cout << text << endl;
